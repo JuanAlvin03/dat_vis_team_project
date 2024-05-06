7746d626ebe454bd5de773824718c699d3a5e3eb
JuanAlvin03 <juanalvinss03@gmail.com>
2024-05-06 11:20:15 +1000

    show world map

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function init(){
     /*CANVAS WIDTH AND HEIGHT*/
-    var w = 500;
-    var h = 300;
+    var w = "150%";
+    var h = 600;
 
     //Get map to focus on Victoria
     var projection = d3.geoMercator()
                     //.center([145, -36.5])
                     //.translate([w / 2, h / 2])
-                    //.scale(2450);
+                    .scale(200);
 
     //Declare the geopath as SVG PATH
     var path = d3.geoPath()
@@ -18,10 +18,23 @@ function init(){
                 .append("svg")
                 .attr("width", w)
                 .attr("height", h)
-                .attr("fill", "grey");
 
     // https://gist.github.com/piwodlaiwo/3734a1357696dcff203a94012646e932
-    // try load CSV
+
+    // try load CSV it works
+    /*
+    d3.csv("data.csv").then(function(data){
+        console.log(data);
+    })
+    */
+
+    d3.json("https://unpkg.com/world-atlas@1/world/110m.json", function(error, world) {
+  if (error) throw error;
+  svg.selectAll("path")
+     .data(topojson.feature(world,world.objects.countries).features)
+     .enter().append("path")
+     .attr("d", path);
+});
 
     // LOAD JSON
     /*
